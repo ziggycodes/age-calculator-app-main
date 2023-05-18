9223f21bee43452e59d701f14329b7a40a8a5427
Isaac Ayodele <ziggycodes25@gmail.com>
2023-05-18 15:09:25 +0100

    completed styles and added styles for active states

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "poppins-regular": ["Poppins-Regular"],
+        "poppins-bold": ["Poppins-Bold"],
+        "poppins-bi": ["Poppins-BoldItalic"],
+        "poppins-eb": ["Poppins-ExtraBold"],
+        "poppins-ebi": ["Poppins-ExtraBoldItalic"],
+        "poppins-italic": ["Poppins-Italic"]
+        
+      }
+    },
   },
   plugins: [],
 }
